f0525422699d477b488c0bd328dcc1b4f282828d
Maria Sokolov <mamizheva85@gmail.com>
2023-09-23 16:24:10 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		image: null
+	}
+
+	async componentDidMount() {
+			const url = "https://random.dog/woof.json";
+			const response = await fetch (url);
+			const data = await response.json();
+			console.log(data)
+			this.setState({image: data.url});
+	}
+	render() {
+		return(
+			<div>
+				<img src={this.state.image} alt="dog" width="300"/>
+			</div>
+		)
+	}
 }
 
 export default App;
